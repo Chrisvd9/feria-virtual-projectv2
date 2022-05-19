bec7a3b612e5c03b575420536d0424d45660da15
Chrisvd9 <christianvd99@gmail.com>
2022-05-19 11:53:53 -0400

    pequeño cambio en home

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
     return (
         <>
             <Componentecarrusel></Componentecarrusel>
-            <h1>Últimos productos posteados</h1>
+            <h1>Últimos productos posteados!</h1>
             {loading ? 
             <Loader/> : error ? <Message variant='danger'>{error}</Message> :  
             <Row>
